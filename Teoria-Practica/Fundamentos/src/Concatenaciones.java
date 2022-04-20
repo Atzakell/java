6aefc2e2412da53310feb2290a4735fb9b0778c5
Ysaac Correa <ysaac356abaddon@gmail.com>
2022-04-20 15:08:30 -0500

    commit aprobbed

--- Teoria-Practica/Fundamentos/src/Concatenaciones.java
+++ Teoria-Practica/Fundamentos/src/Concatenaciones.java
@@ -5,9 +5,23 @@ public class Concatenaciones {
 
         var union = titulo + user;
         System.out.println("union -> "+ union);
-
+        
         var i=10;
         var j=7;
         System.out.println(union +" "+ (i + j) + " años" );
+        
+        var h=1;
+        var k=8;
+        System.out.println(union +" "+ h + k + " años" );
+    
+    //?caracteres especiales
+        var nom = "Aysaac";
+
+        System.out.println("Nueva Linea: \n" + nom);
+        System.out.println("Tabulador: \t" + nom);
+        System.out.println("Retroceso: \b\b" + nom);
+        System.out.println("Comilla simple: \'" + nom + "\'");
+        System.out.println("Comilla Doble: \"" + nom + "\"");
+
     }
 }
