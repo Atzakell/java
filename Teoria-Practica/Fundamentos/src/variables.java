81114414e874f11560ebfedd98db16034964bc17
Ysaac Correa <ysaac356abaddon@gmail.com>
2022-04-20 14:03:07 -0500

    Atzakell changed

--- Teoria-Practica/Fundamentos/src/variables.java
+++ Teoria-Practica/Fundamentos/src/variables.java
@@ -1,6 +1,21 @@
 public class variables {
     public static void main(String args[]) {
+        //?asigando valore entero e imprimiendo
         int unaVariableEntera = 100;
         System.out.println(unaVariableEntera);
+
+        unaVariableEntera=unaVariableEntera-42;
+        System.out.println(unaVariableEntera);
+        
+        //?asigando valore cadena e imprimiendo
+        String unaVariableCadena = "Nuggets de pollo";
+        System.out.println(unaVariableCadena);
+
+        unaVariableCadena = "Twister Americano";
+        System.out.println(unaVariableCadena);
+
+        //* var - Inferencia de tipos en java
+        var otraVariableEntera = 69;
+        System.out.println(otraVariableEntera);
     }
 }
