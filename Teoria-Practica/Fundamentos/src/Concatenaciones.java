e7927da9fd3e79e9d074de2cd304daeb1d9ea51e
Ysaac Correa <ysaac356abaddon@gmail.com>
2022-04-20 14:33:55 -0500

    commit aprobbed

---
+++ Teoria-Practica/Fundamentos/src/Concatenaciones.java
@@ -0,0 +1,9 @@
+public class Concatenaciones {
+    public static void main(String args[]) {
+        var user = "Ysaac";
+        var titulo = "Ingeniero de Software ";
+
+        var union = titulo + user;
+        System.out.println("union -> "+ union);
+    }
+}
--- Teoria-Practica/Fundamentos/src/variables.java
+++ Teoria-Practica/Fundamentos/src/variables.java
@@ -1,5 +1,3 @@
-import javax.print.attribute.SupportedValuesAttribute;
-
 public class variables {
     public static void main(String args[]) {
         //?asigando valore entero e imprimiendo
@@ -23,5 +21,14 @@ public class variables {
         var mismaVariableEntera2 = "0la infiel";
         System.out.println(mismaVariableEntera2);
         
+        //!Valores permitidos
+        var myvarb = 1;
+        var _myvarb = 2;
+        var $myvarb = 3;
+        System.out.println(myvarb);
+        System.out.println(_myvarb);
+        System.out.println($myvarb);
+        //var ámyvarb = 43; no es lo ideal
+        //var #myvarb = 23; no se permiten caracteres especiales 
     }
 }
