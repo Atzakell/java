adde8d8a623c2eaad462a5bac230ff6f55d12b69
Atzakell <ysaac356abaddon@gmail.com>
2022-05-11 17:28:57 -0500

    commit atza

--- Teoria-Practica/Fundamentos/src/Scanneo.java
+++ Teoria-Practica/Fundamentos/src/Scanneo.java
@@ -2,14 +2,24 @@ import java.util.Scanner;
 
 public class Scanneo {
     public static void main(String args[]) {
+        //Ejemplo
         System.out.println("Escribe tu nombre: ");
-        Scanner consola = new Scanner(System.in);
+        //el scanner funciona de manera global solo se requiere usarlo una vez
+        Scanner consola = new Scanner(System.in); 
         var alumno = consola.nextLine();
         System.out.println("Alumno = " + alumno);
 
         System.out.println("Escribe tu profesion: ");
         var titulo =  consola.nextLine();
         System.out.println("Resultado: "+ titulo + " " + alumno);
+        
+        //!ejercicio
+        System.out.println("Proporciona un titulo-> ");
+        var tema = consola.nextLine();
+        
+        System.out.println("Proporciona el autor-> ");
+        var creador =consola.nextLine();
 
+        System.out.println(tema + " escrito por " + creador);
     }
 }
