627b9adde2545a865ab3719ca1db8328e3bccb26
Atzakell <ysaac356abaddon@gmail.com>
2022-06-04 13:20:35 -0500

    actualizacion java

--- Teoria-Practica/Fundamentos/src/Scanneo.java
+++ Teoria-Practica/Fundamentos/src/Scanneo.java
@@ -2,24 +2,26 @@ import java.util.Scanner;
 
 public class Scanneo {
     public static void main(String args[]) {
-        //Ejemplo
+        // Ejemplo
         System.out.println("Escribe tu nombre: ");
-        //el scanner funciona de manera global solo se requiere usarlo una vez
-        Scanner consola = new Scanner(System.in); 
-        var alumno = consola.nextLine();
-        System.out.println("Alumno = " + alumno);
+        try (// el scanner funciona de manera global solo se requiere usarlo una vez
+                Scanner consola = new Scanner(System.in)) {
+            var alumno = consola.nextLine();
+            System.out.println("Alumno = " + alumno);
 
-        System.out.println("Escribe tu profesion: ");
-        var titulo =  consola.nextLine();
-        System.out.println("Resultado: "+ titulo + " " + alumno);
-        
-        //!ejercicio
-        System.out.println("Proporciona un titulo-> ");
-        var tema = consola.nextLine();
-        
-        System.out.println("Proporciona el autor-> ");
-        var creador =consola.nextLine();
+            System.out.println("Escribe tu profesion: ");
+            var titulo = consola.nextLine();
+            System.out.println("Resultado: " + titulo + " " + alumno);
 
-        System.out.println(tema + " escrito por " + creador);
+            // !ejercicio
+            System.out.println("Proporciona un titulo-> ");
+            var tema = consola.nextLine();
+
+            System.out.println("Proporciona el autor-> ");
+            var creador = consola.nextLine();
+
+            System.out.println(tema + " escrito por " + creador);
+        }
+
     }
 }
